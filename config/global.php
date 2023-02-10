13901b025416acdfc3ede0c09ba010fa08e5f792
RMalanco <rmalanco@rmalanco.com>
2023-02-10 02:50:23 -0600

    actualizacion en constante de base de datos

--- config/global.php
+++ config/global.php
@@ -1,6 +1,6 @@
 <?php
 define("DB_HOST", "localhost");
-define("DB_NAME", "test");
+define("DB_NAME", "ADVentas");
 define("DB_USERNAME", "root");
 define("DB_PASSWORD", "");
 define("DB_ENCODE", "utf8");
